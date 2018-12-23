69f526b7376c553f8d0e099dd9adcfd070ffd703
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2018-12-23 10:47:46 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(name='Telecontrol',  #打包后的包文件名
       version='1.0',  #版本
       description='a Telecontrol',   
       author=' Mr.yan',   
+      license="Apache-2.0",
       author_email=' mryan050415@gmail.com',   
       url=' https://github.com/SuperSystemStudio/TelecontrolCodeRepairwoman',   
       py_modules=['TelecontrolCodeRepairwoman'],  #与前面的新建文件名一致 
