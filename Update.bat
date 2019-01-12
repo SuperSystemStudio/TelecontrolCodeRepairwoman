1505ca30debe3e8f58f0109822c0df3a7102a1af
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2019-01-12 20:18:34 +0800

    update

--- Update.bat
+++ Update.bat
@@ -3,3 +3,5 @@ rd /s/q dist
 python setup.py sdist
 twine upload dist/*
 rd /s/q dist
+pip uninstall Telecontrol
+pip install Telecontrol
--- main.py
+++ main.py
@@ -2,14 +2,27 @@ import socket
 
 client = socket.socket()
 print('please input the ip')
-ip = input()
+ip = input('ip: ')
 print('please input the path')
-path=int(input())
+path=int(input("path: "))
 client.connect((ip,path))
+mod = 0
 while True:
-    msg = input(">> ").strip()
-    if len(msg) == 0:continue
-    client.send( msg.encode("utf-8") )
+    if mod == 0:
+        msg = input("[user]" ).strip()
+    elif mod == 1:
+        msg = input("[root]" ).strip()
+    if len(msg) == 0:
+        continue
+    client.send(msg.encode("utf-8"))
     data = client.recv(1024)
     print(data.decode()) #命令执行结果
+    if data == b'please input the password':
+        password = input('password: ').strip()
+        if len(password) == 0:
+            continue
+    client.send(password.encode("utf-8"))
+    data = client.recv(1024)
+    if data == b'now,you are root!':
+        mod = 1
 client.close()
